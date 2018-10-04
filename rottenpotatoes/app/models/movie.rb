b14978d7dbb7c2e0c8a147d2f89d023f5f423aa2
EC2 Default User <ec2-user@ip-172-31-4-121.us-east-2.compute.internal>
2018-10-04 04:11:57 +0000

    Ready to push to heroku

--- rottenpotatoes/app/models/movie.rb
+++ rottenpotatoes/app/models/movie.rb
@@ -4,9 +4,12 @@ class Movie < ActiveRecord::Base
   end
   
   def movies_with_same_director
-    puts "Director is #{director}"
     all_movies = Movie.all
     movies = []
+    if director == "" || director == nil
+      # No director field, return nil
+      return nil
+    end
     all_movies.each do |movie| 
       if movie.director == director && movie.title != title
         movies.push(movie)
